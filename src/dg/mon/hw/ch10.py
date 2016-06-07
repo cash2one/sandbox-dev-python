936896ceb14930511eaf200faf3a98914b65343f
xenron <xenron@hotmail.com>
2016-06-07 21:29:09 +0900

    msg

---
+++ src/dg/mon/hw/ch10.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#encoding:utf-8
+import xlwt
+
+def txt2excel(txt_filename, excel_filename):
+
+    f = open(txt_filename)             # get file object
+    line = f.readline()
+    line_index = 0
+
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('sheet1', cell_overwrite_ok=True)
+
+    while line:
+        print line,                 # end by ',' will ignore /r/n
+        # print(line, end = '')     # can be work in Python 3
+
+        cell_index = 0
+        for cell in line.replace("\n", "").split("\t"):
+            sheet1.write(line_index, cell_index, cell)
+            cell_index += 1
+
+        line = f.readline()
+        line_index += 1
+
+    f.close()
+    workbook.save(excel_filename)
+
+if __name__ == '__main__':
+    txt2excel('d:/tmp/data1.txt', 'd:/tmp/myexcel.xls')
